6b5d226be63b5c1f152838bb301877617991ff7a
jn <1287601638@qq.com>
2018-11-27 20:40:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FootGuide/>
+  </div>
+</template>
+
+<script>
+import FootGuide from './components/FootGuide/FootGuide'
+export default {
+  components:{
+    FootGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+
+</style>
